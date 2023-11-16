e5d73872de80f96049ac53fac355ab38b18eb4b9
breadforhire <126725193+breadforhire@users.noreply.github.com>
2023-11-16 17:53:29 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "immintrin.h"
 #include "emmintrin.h"
-
+#include "avx2intrin.h"
+#include "bucket.h"
 /* This is going to a one month project in which we will implement the 64 bit granger moss primes in tables */
 
 #define _LEN 8
@@ -9,9 +10,11 @@
 #define ADD(A, B) _mm256_add_epi32(A, B)
 #define SUB(A, B) _mm256_sub_epi32(A, B)
 #define XOR(A, B) _mm256_xor_si256(A, B)
-#define MUL(A,B) _mm256_mul_ps(A, B)
 
-/*[x0y0 + x1y8 + x2y7 + x3y6 + x4y5 + x5y4 + x6y3 + x7y2 + x8y1,
+
+/*
+[
+x0y0 + x1y8 + x2y7 + x3y6 + x4y5 + x5y4 + x6y3 + x7y2 + x8y1,
 x0y1 + x1y0 + x2y8 + x3y7 + x4y6 + x5y5 + x6y4 + x7y3 + x8y2,
 x0y2 + x1y1 + x2y0 + x3y8 + x4y7 + x5y6 + x6y5 + x7y4 + x8y3,
 x0y3 + x1y2 + x2y1 + x3y0 + x4y8 + x5y7 + x6y6 + x7y5 + x8y4,
@@ -46,15 +49,18 @@ static const int32_t r_y [8] = {1297, 1601, 2917, 3137, 4357, 5477, 7057, 8101};
 
 void E()
 {
+  
+
+  
   __m256i e_x = _mm256_load_si256((__m256i*) r_x);
  __m256i e_y = _mm256_load_si256((__m256i*) r_y);
 
- for(;;)
- {
 
-  /*will add later*/
+                                     // --> bucket  
+ /*x0y0 + x1y8 + x2y7 + x3y6 + x4y5 + x5y4 + x6y3 + x7y2 + x8y */
 
- }
+ __m256i arthox =_mm256_mulhi_epi16(e_x, e_y);  
+// fill(bucket, &arthox);
 
 
 
@@ -62,5 +68,5 @@ void E()
 void main()
 {
 
-
+ E();
 }
