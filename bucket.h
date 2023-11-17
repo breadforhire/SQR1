3ea976e3ae4a7e4b23817bb1572d159e378571b7
breadforhire <126725193+breadforhire@users.noreply.github.com>
2023-11-16 21:07:12 -0500

    Update bucket.h

--- bucket.h
+++ bucket.h
@@ -1,17 +1,22 @@
 #include "stdint.h"
 #include <stdlib.h>
 #include "string.h"
+#include "immintrin.h"
+#include "emmintrin.h"
+#include "avx2intrin.h"
+#include <smmintrin.h>
 
-struct bucket;
+struct bucket *bucket;
 
-extern struct bucket bucket;
+
+extern struct bucket *bucket;
 
 /*starts the first bucket*/
-void *init(struct bucket, ssize_t capacity );
+void *init(ssize_t capacity );
 
 
 /* fills the bucket */
-void *fill(struct bucket *bucket, void* data);
+void *fill(struct bucket *bucket, __m256i data);
 
 /*emptys the bucket*/
 void *empty(struct bucket *bucket);
