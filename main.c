538b18dd5682173134302912017eafde594f7133
breadforhire <126725193+breadforhire@users.noreply.github.com>
2023-11-17 20:02:12 -0500

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,8 @@
 #include "bucket.h"
 /* This is going to a one month project in which we will implement the 64 bit granger moss primes in tables */
 
-#define _LEN 8
+#define length 8
+
 /*
 [
 x0y0 + x1y1 ...
@@ -21,20 +22,21 @@ x0y6 + x1y5 + x2y4 + x3y3 + x4y2 + x5y1 + x6y0 + x7y8 + x8y7,
 x0y7 + x1y6 + x2y5 + x3y4 + x4y3 + x5y2 + x6y1 + x7y0 + x8y8,
 x0y8 + x1y7 + x2y6 + x3y5 + x4y4 + x5y3 + x6y2 + x7y1 + x8y0].  */
 
+/*The table above will be z ≡ x y(mod t9 − 1)   and will contain our base values to actually run the program*/
+ /*the buckets are filled in reverse for example the first index is x0y8 + x1y7 + x2y6 + x3y5 + x4y4 + x5y3 + x6y2 + x7y1 + x8y0 */
 
 
-/*1 /  8 */
 
 
-//#define A(z0, z1, )  ADD(z0, z1)
+
+/*we need align each int32_t array*/
 
 
-/* alignment */
 __attribute__((__aligned__(32)))
-static const int32_t r_y[8] = {2, 4, 8, 16, 32, 64, 128, 256};
-/* alignment */
+static const int32_t x_n[8] = {2, 4, 8, 16, 32, 64, 128, 256};
+
 __attribute__((__aligned__(32)))
-static const int32_t r_x [8] = {2, 4, 8, 16, 32, 64, 128, 256};
+static const int32_t y_n [8] = {2, 4, 8, 16, 32, 64, 128, 256};
 
 __attribute__((__aligned__(32)))
 static const int32_t twos [8] = {2, 2, 2, 2, 2, 2, 2, 2};
@@ -46,48 +48,46 @@ void A(struct table *table)
 
 
  /*load resiude*/
- __m256i e_x = _mm256_load_si256((__m256i*) r_x);
- __m256i e_y = _mm256_load_si256((__m256i*) r_y);
+ __m256i e_x = _mm256_load_si256((__m256i*) x_n);
+ __m256i e_y = _mm256_load_si256((__m256i*) y_n);
  __m256i z0;
  __m256i z1;
- __m256i c = _mm256_load_si256((__m256i*) twos);
+
+ __m256i constant_two = _mm256_load_si256((__m256i*) twos);
 
  __m256i t;
 
- /*init eight buckets for each row*/
- init(_LEN);
 
 
 
 
-                                     // --> bucket
- /*x0y0 + x1y8 + x2y7 + x3y6 + x4y5 + x5y4 + x6y3 + x7y2 + x8y1 */
+
+ /* bucket the entire x0y0 + x1y8 + x2y7 + x3y6 + x4y5 + x5y4 + x6y3 + x7y2 + x8y1  as*(x0y8 x1y7 x2y6 x3y5) +  (x4y4  x5y3  x6y2 x7y1  x8y0)  */
  /*shift then add to the bucket*/
- /*_LEN will be our bucket length (there is only one bucket due to the barebones project)*/
 
- for(int i = 0; i < _LEN; ++i)
+ for(int i = 0; i < length; ++i)
  {
-  /* fill up buckets and call it a day */
+
    z0 = _mm256_mullo_epi16(e_x, e_y);
    z1 = _mm256_mulhi_epi16(e_x, e_y);
-  _mm256_slli_epi32(e_y, 1) ;
-  /*fill every bucket with vector z0 + z1 */
+   
+   /*shifts the entire vector*/
+   e_y = _mm256_slli_epi64(e_y, 1) ;
 
-  /*(x0y8 x1y7 x2y6 x3y5) +  (x4y4  x5y3  x6y2 x7y1  x8y0) */
-
-  // table -> bucket[i] = _mm256_add_epi64(z0, z1);
+  /*fill every bucket with vector  (x0y8 x1y7 x2y6 x3y5) +  (x4y4  x5y3  x6y2 x7y1  x8y0)  */
+  fill(table, _mm256_add_epi64(z0, z1), i);
  }
 
  /*the buckets are filled in reverse for example the first index is x0y8 + x1y7 + x2y6 + x3y5 + x4y4 + x5y3 + x6y2 + x7y1 + x8y0 */
-
  /*t1 = 2(x0x8 + x1x7 + x2x6 + x3x5) + x4^2 */
- t = _mm256_mulhi_epi16(c, table -> bucket[_LEN % _LEN]);
+
+ t = _mm256_mulhi_epi16(constant_two, extract(table, length % length));
 
 }
 
 void main()
 {
- struct table *t;
+ struct table* t = init(length);
  A(t);
 
 }
