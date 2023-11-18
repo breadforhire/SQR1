91b6218605d31dc0004c70ae90c9628623865417
breadforhire <126725193+breadforhire@users.noreply.github.com>
2023-11-17 20:02:49 -0500

    Update bucket.h

--- bucket.h
+++ bucket.h
@@ -1,6 +1,4 @@
-#include "stdint.h"
 #include <stdlib.h>
-#include "string.h"
 #include "immintrin.h"
 #include "emmintrin.h"
 #include "avx2intrin.h"
@@ -9,37 +7,13 @@
 
 
 
-struct table
-{
-
-
- /*size of the table*/
- ssize_t size;
-
- /*the capacity for the entire */
- ssize_t capacity;
-
- __m256i bucket[8];
-
- /*allocation for memory size*/
- ssize_t alloc;
-
-} ;
-
-struct table *table;
-
-
-extern struct table *table;
-
-
-
 /*starts the first bucket*/
-void *init(ssize_t capacity );
+extern struct table* init(ssize_t capacity );
 
 
 /* fills the bucket */
-void *fill(struct table *table, __m256i data, int key);
+extern void *fill(struct table *table, __m256i data, int key);
 
 
-/*checks if table is empty*/
-void *empty(struct table *table);
+/*extract*/
+extern __m256i extract(struct table *table, int key);
