afa3bc9bfb7a78c400c7ecb48607407c8797aa27
breadforhire <126725193+breadforhire@users.noreply.github.com>
2023-11-21 20:49:17 -0500

    Update main.c

--- main.c
+++ main.c
@@ -22,10 +22,10 @@
         memset(&result, 0x00, length); \
     } while (0)
 
-#define UNROLL(vector, index, vector_assignment, index_assignment, mod) \
+#define UNROLL(vector, index, vector_assignment, index_assignment, mod, filler) \
     do { \
-        mod = _mm256_and_si256(vector, _mm256_sub_epi32(mod, _mm256_set1_epi32(1))); \
-        vector_assignment[index_assignment] = _mm256_extract_epi32(mod, 0); \
+        filler = _mm256_and_si256(vector, _mm256_sub_epi32(mod, _mm256_set1_epi32(1))); \
+        vector_assignment[index_assignment] = _mm256_extract_epi32(filler, 0); \
     } while (0)
 
 #define PRINT(vector) \
@@ -104,16 +104,22 @@ void A(struct table *table)
  __m256i z1;
  __m256i buckets[length];
 
-
+ __m256i filler = _mm256_set1_epi32(1);
  __m256i constant_two = _mm256_load_si256((__m256i*) twos);
  __m256i constant_sixteen_e2 = _mm256_load_si256((__m256i*) sixteen_e_2 );
  __m256i constant_fours =_mm256_load_si256((__m256i*) fours);
 
 
 
+ /*vectors that will be applied but not used for storage*/
  __m256i t0;
  __m256i t1;
- __m256i t2;
+
+
+ __m256i t0_storage;
+ __m256i t1_storage;
+ __m256i t2_storage;
+ __m256i z0_storage;
 
 
 
@@ -149,25 +155,40 @@ void A(struct table *table)
 
 
  }
- //start, end, buckets, vector, bucket_index, storeindex arr
+ /*t1 = 2(x0x8 + x1x7 + x2x6 + x3x5) + x4 ^2*/
+ /*start, end, buckets, vector, bucket_index, storeindex arr*/
  ROLL(0, 4, buckets, t1, length - 1, 0, result);
  t1 = _mm256_mullo_epi32(t1, constant_two);
  t1 = _mm256_add_epi32(t1, constant_sixteen_e2);
-/*vector, index, vector_assignment, index_assignment, mod*/
- UNROLL(t1, 0, t0, 0, constant_two);
-
-
- /*store the mods in t0 and store the vector operations in t1*/
-
- ROLL(1, 5, buckets, t2, 0, 0, result);
- t2 = _mm256_mullo_epi32(t2, constant_fours);
- t2 = _mm256_add_epi32(t2,  constant_sixteen_e2);
- t2 = _mm256_add_epi32(t2, _mm256_mullo_epi32(_mm256_set1_epi32(_mm256_extract_epi32(t1, 0) >> 58), constant_two)) ;
  /*vector, index , vector_assignment, index_assignment*/
- STORE(t2, 0, t1 , 1);
- /*vector, index, vector_assignment, index_assignment, mod*/
- UNROLL(t2, 0, t0, 1, constant_two);
- PRINT(t1);
+ STORE(t1, 0, t1_storage, 0);
+
+
+/*vector, index, vector_assignment, index_assignment, mod*/
+/*takes the mod of t1[0] by constant two and stores it in t0_storage at index 0*/
+/*t0 = t1 mod t*/
+/*the storage is already built into unroll so we do not have to worry about it*/
+ UNROLL(t1_storage, 0, t0_storage, 0, constant_two, filler);
+
+
+ /*t2 = 4(x1x8 + x2x7 + x3x6 + x4x5) + x0^2 + 2(t1 >> 58)*/
+ ROLL(1, 5, buckets, t1, 0, 1, result);
+ t1 = _mm256_mullo_epi32(t1, constant_fours);
+ t1 = _mm256_add_epi32(t1,  constant_fours);
+ t1 = _mm256_add_epi32(t1, _mm256_mullo_epi32(_mm256_set1_epi32(_mm256_extract_epi32(t1_storage, 0) >> 58), constant_two)) ;
+
+ STORE(t1, 2, t2_storage, 0);
+ UNROLL(t2_storage, 0, z0, 0, constant_two, filler);
+
+ PRINT(z0);
+
+ ROLL(3, 6, buckets, t1, 1, 0, result);
+ t1 = _mm256_mullo_epi32(t1, constant_fours);
+ ROLL(0, 1, buckets, t1, 1, 1, result);
+ t1 = _mm256_add_epi32(t1, _mm256_mullo_epi32(_mm256_set1_epi32(&t1[1]), constant_two));
+
+
+
 
 
 
